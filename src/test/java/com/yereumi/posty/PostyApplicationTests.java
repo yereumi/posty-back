0bfb13560ee8398dece9024a23937456e09abde8
yereumi <lovelyyeji01@gmail.com>
2025-04-09 11:43:37 +0900

    test: 테스트용 application-test.yml 추가 및 프로파일 분리

--- src/test/java/com/yereumi/posty/PostyApplicationTests.java
+++ src/test/java/com/yereumi/posty/PostyApplicationTests.java
@@ -2,8 +2,10 @@ package com.yereumi.posty;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 class PostyApplicationTests {
 
 	@Test
