0c5db00b08be2f70b11121d063f43a3d00306f89
yereumi <lovelyyeji01@gmail.com>
2025-04-09 09:08:51 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/yereumi/posty/PostyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yereumi.posty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
