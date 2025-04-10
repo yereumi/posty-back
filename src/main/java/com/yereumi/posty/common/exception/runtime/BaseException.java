64d96607eeba83c46d49e0d807cc063b37f11225
yereumi <lovelyyeji01@gmail.com>
2025-04-10 11:21:55 +0900

    feat: BaseException 작성

---
+++ src/main/java/com/yereumi/posty/common/exception/runtime/BaseException.java
@@ -0,0 +1,14 @@
+package com.yereumi.posty.common.exception.runtime;
+
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+
+    private final String code;
+
+    public BaseException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+}
